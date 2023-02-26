1e97c4be1edaaa3fd72f4b3a9f0fbc8418a4bb1c
Owono Philippe Brice <brice.mich@gmail.com>
2023-02-26 18:33:42 +0100

    Update Hello.java for fun
    just test jenkins

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World : I'm learning jenkins");
+      System.out.println("Hello, World : I'm learning jenkins to be a DevOps");
    }
 }
